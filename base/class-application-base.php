c039fa03775495267d081edd59fdd81064a027be
Mike Schinkel <mike@newclarity.net>
2013-12-01 23:09:51 -0500

    Renamed Exo_Webapp_Base to Exo_Application_Base

---
+++ base/class-application-base.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Class Exo_Application_Base
+ *
+ */
+abstract class Exo_Application_Base extends Exo_Main_Base {
+}
--- base/class-webapp-base.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Class Exo_Webapp_Base
- *
- */
-abstract class Exo_Webapp_Base extends Exo_Main_Base {
-}
