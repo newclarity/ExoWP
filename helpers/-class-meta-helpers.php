31e8b46b07ef23ec724afb4c6c59e05a1172879c
Mike Schinkel <mike@newclarity.net>
2013-12-08 23:09:02 -0500

    Registration of the "helper" class _Exo_Meta_Helpers, added so we can isolate the apply_meta_prefix() functionality.

---
+++ helpers/-class-meta-helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Class _Exo_Meta_Helpers
+ *
+ * Helpers that help with Post meta, User meta, etc.
+ *
+ */
+class _Exo_Meta_Helpers extends Exo_Helpers_Base {
+
+  /**
+   * Applies a prefix to post/user/etc meta field names.
+   *
+   * This is a global function because it should come from the implementation (app/website/plugin/theme.)
+   *
+   * @param string $meta_name
+   * @return string
+   *
+   * @todo Implement this somehow
+   */
+  static function apply_meta_prefix( $meta_name ) {
+    if ( isset( $meta_name[0] ) && '_' != $meta_name[0] )
+      $meta_name = "_{$meta_name}";
+    return $meta_name;
+  }
+
+}
--- on-load.php
+++ on-load.php
@@ -1,7 +1,13 @@
 <?php
 /**
- * File: /helpers/class-helpers.on-load.php
+ * File: /helpers/-class-helpers.on-load.php
  */
 
 Exo::register_helper( '_Exo_Helpers' );
+
+/**
+ * File: /helpers/-class-meta-helpers.on-load.php
+ */
+
+Exo::register_helper( '_Exo_Meta_Helpers' );
 
